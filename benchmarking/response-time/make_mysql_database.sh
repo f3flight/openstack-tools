ff343e5fa9f078810a498d9b34146f975ba18fff
root <root@node-5.(none)>
2015-08-21 21:55:39 +0000

    Adds more flexibility

--- benchmarking/response-time/make_mysql_database.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-mysql -e "create database metadata"
-mysql -D metadata -e "create table results ( id INT NOT NULL AUTO_INCREMENT, timestamp DATETIME, nanos INT, responce_time DOUBLE, error BOOLEAN, PRIMARY KEY (id) )"
